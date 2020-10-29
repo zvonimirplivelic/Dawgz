4e5037fa655ef3978f1ed18df1c4cfb6ea7ee4b0
Zvonimir Plivelić <zvonimirplivelic91@gmail.com>
2020-10-29 17:05:34 +0100

    data binding implemented on dog item, need to solve challenge

---
+++ app/src/main/java/com/zvonimirplivelic/dawgz/view/DogClickListener.kt
@@ -0,0 +1,7 @@
+package com.zvonimirplivelic.dawgz.view
+
+import android.view.View
+
+interface DogClickListener  {
+    fun onDogClicked(v: View)
+}
--- app/src/main/java/com/zvonimirplivelic/dawgz/view/DogsListAdapter.kt
+++ app/src/main/java/com/zvonimirplivelic/dawgz/view/DogsListAdapter.kt
@@ -3,16 +3,18 @@ package com.zvonimirplivelic.dawgz.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.zvonimirplivelic.dawgz.R
+import com.zvonimirplivelic.dawgz.databinding.ItemDogListBinding
 import com.zvonimirplivelic.dawgz.model.DogBreed
 import com.zvonimirplivelic.dawgz.util.getProgressDrawable
 import com.zvonimirplivelic.dawgz.util.loadImage
 import kotlinx.android.synthetic.main.item_dog_list.view.*
 
 class DogsListAdapter(val dogsList: ArrayList<DogBreed>) :
-    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(), DogClickListener {
 
     fun updateDogList(newDogsList: List<DogBreed>) {
         dogsList.clear()
@@ -22,27 +24,40 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_dog_list, parent, false)
+//        val view = inflater.inflate(R.layout.item_dog_list, parent, false)
+        val view = DataBindingUtil.inflate<ItemDogListBinding>(
+            inflater,
+            R.layout.item_dog_list,
+            parent,
+            false
+        )
         return DogViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-        holder.view.name.text = dogsList[position].dogBreed
-        holder.view.lifespan.text = dogsList[position].lifeSpan
+        holder.view.dog = dogsList[position]
+        holder.view.listener = this
+//        holder.view.name.text = dogsList[position].dogBreed
+//        holder.view.lifespan.text = dogsList[position].lifeSpan
+//        0
+//        holder.view.setOnClickListener {
 
-        holder.view.setOnClickListener {
-            val action = DogListFragmentDirections.actionDetailFragment()
-            action.dogUuid = dogsList[position].uuid
-            Navigation.findNavController(it)
-                .navigate(action)
-        }
-        holder.view.imageView.loadImage(
-            dogsList[position].imageUrl,
-            getProgressDrawable(holder.view.imageView.context)
-        )
+//        }
+//        holder.view.imageView.loadImage(
+//            dogsList[position].imageUrl,
+//            getProgressDrawable(holder.view.imageView.context)
+//        )
+    }
+
+    override fun onDogClicked(v: View) {
+        val uuid = v.dogId.text.toString().toInt()
+        val action = DogListFragmentDirections.actionDetailFragment()
+        action.dogUuid = uuid
+        Navigation.findNavController(v)
+            .navigate(action)
     }
 
     override fun getItemCount() = dogsList.size
 
-    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class DogViewHolder(var view: ItemDogListBinding) : RecyclerView.ViewHolder(view.root)
 }
