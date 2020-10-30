e2ea15d650d2b5e55ba5fbbc86394f1b09916be9
Zvonimir Plivelić <zvonimirplivelic91@gmail.com>
2020-10-30 14:49:53 +0100

    added sharing intent

--- app/src/main/java/com/zvonimirplivelic/dawgz/view/DogDetailFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/dawgz/view/DogDetailFragment.kt
@@ -115,7 +115,12 @@ class DogDetailFragment : Fragment() {
             }
 
             R.id.action_share -> {
-
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Check out this dog breed")
+                intent.putExtra(Intent.EXTRA_TEXT, "${currentDog?.dogBreed} is bred for ${currentDog?.bredFor}")
+                intent.putExtra(Intent.EXTRA_STREAM, currentDog?.imageUrl)
+                startActivity(Intent.createChooser(intent, "Share with: "))
             }
         }
         return super.onOptionsItemSelected(item)
